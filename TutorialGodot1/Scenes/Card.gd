7035acd7165387283252672aa31157b8b7d85f8c
Jorge_dany <spumasc11@gmail.com>
2023-07-11 07:27:31 -0600

    Primer commit de juego base funcional, con historia de prueba
    El commit es en formato Portrail para celulares, las imagenes y el fondo cambian, así como es funcional la toma de decisiones.
    
    La historia está configurada para el script del documento de prueba.

---
+++ TutorialGodot1/Scenes/Card.gd
@@ -0,0 +1,48 @@
+extends Container
+
+class_name Card
+
+var linkChar = ""
+var bgCikir = ""
+var resIzq = ""
+var resDer = ""
+
+func _ready():
+	changeAlphaDer(0)
+	changeAlphaIzq(0)
+
+func infoCard(resp1,resp2,bgColor, linkImg):
+	changeAlphaIzq(1)
+	changeAlphaDer(1)
+	if(linkImg!=null):
+		setImg(linkImg)
+	if(bgColor!=null):
+		$Front.self_modulate = Color("#"+bgColor)
+		print("Color: #"+bgColor)
+	$Front/Content/RespDer/txtRespDer.text=resp1
+	$Front/Content/RespIzq/txtRespIzq.text=resp2
+
+func flipFront():
+	$Back.visible=false
+	$Front.visible=true
+
+func changeAlphaIzq(alp):
+	var color=$Front/Content/RespIzq.modulate
+	color.a=alp
+	$Front/Content/RespIzq.modulate=color
+	
+func changeAlphaDer(alp):
+	var color=$Front/Content/RespDer.modulate
+	color.a=alp
+	$Front/Content/RespDer.modulate=color
+	
+func flipBack():
+	$Back.visible=true
+	$Front.visible=false
+	
+func setImg(urlImg):
+	$Front/Content.texture=load("res://Sprites/"+urlImg) as Texture 
+
+func setBgColor(colorS):
+	$Front/Content.self_modulate=Color("#"+colorS)
+	pass
---
+++ TutorialGodot1/Scenes/UIControl.gd
@@ -0,0 +1,29 @@
+extends Control
+
+var isPause=false
+
+
+func _ready():
+	$Menu.visible=false
+
+func _on_btn_menu_pressed():
+	isPause=true
+	swPause()
+
+func _input(event):
+	if event is InputEventKey:
+		if Input.is_action_just_pressed("ui_menu"):
+			isPause=!isPause
+			swPause()
+func swPause():
+	if (isPause):
+		$Menu.visible=true
+		$HUD.visible=false
+	else:
+		$Menu.visible=false
+		$HUD.visible=true
+
+
+func _on_continuar_pressed():
+	isPause=false
+	swPause()
